abe281fc1e34ec62170bc378b9aeeacf097c64d2
husnain-nawaz <chhusnain2345@gmail.com>
2021-03-18 19:36:17 +0500

    html+javscript

---
+++ html/1.file_extract.js
@@ -0,0 +1,142 @@
+
+
+// function myfunction() {
+//     document.getElementById('text1').innerHTML = "hello";
+
+// }
+
+
+// ///////////////////////////////////////////////////////
+
+function func2(){
+    document.getElementById('text2').innerHTML =5+6;
+
+}
+
+
+document.write(34+65);
+
+
+
+
+function func3(){
+    document.write(1+1);
+}
+
+// alert
+
+
+// window.alert(1+12);
+
+// alert(1+3);
+
+
+
+
+console.log(5+7);
+
+
+
+// print
+
+function func4(){
+    window.print();
+}
+
+// sum
+
+var a, b, c;
+
+a = 10;
+b = 15;
+c = a+b;
+
+function func5(){
+    document.getElementById('sum1').innerHTML = "value of c " +c+ ".";
+
+}
+
+
+
+var x,y,z;
+x=1;y=2;z=x+y;
+function func6(){
+    document.getElementById('sum2').innerHTML = z;
+}
+
+
+
+// multiply
+
+function func7(){
+    document.getElementById('multiply1').innerHTML = (1+5)*7;
+    document.getElementById('multiply1').innerHTML = "johny"+ " " + "sir";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
